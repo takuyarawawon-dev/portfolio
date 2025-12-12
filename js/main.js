2f16129f2ad427c7ac287078edf6902f1e91d9bd
takuyarawawon-dev <takuyarawawon.dev@gmail.com>
2025-12-13 04:10:22 +0900

    init commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  body.classList.add("dark");
+}
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleButton.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleButton.textContent = "🌙";
+  }
+});
